065f1085187f0465959d3c3e741ca6813babb3ba
AhhadQamar <ahadqam1@gmail.com>
2026-09-19 15:35:12 +0500

    Disable signups in hosts/licht configuration
    Changed:
    hosts/licht/configuration.nix

--- hosts/licht/configuration.nix
+++ hosts/licht/configuration.nix
@@ -56,7 +56,7 @@
       DOMAIN = "http://127.0.0.1:8000";
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 8000;
-      SIGNUPS_ALLOWED = true;
+      SIGNUPS_ALLOWED = false;
     };
   };
 
